1dc973571e2180be47c234bcbc9b4fba5b18ea71
otf <otf@me.com>
2024-08-10 13:53:28 +0900

    custom nci.crates

--- src-tauri/crates.nix
+++ src-tauri/crates.nix
@@ -5,42 +5,53 @@
     ...
   }: let
     crateName = "tauri-app";
+    packages = with pkgs; [
+      curl
+      wget
+      pkg-config
+      dbus
+      openssl_3
+      glib
+      gtk3
+      libsoup
+      webkitgtk
+      librsvg
+    ];
+    libraries = with pkgs; [
+      webkitgtk
+      gtk3
+      cairo
+      gdk-pixbuf
+      glib
+      dbus
+      openssl_3
+      librsvg
+    ];
   in {
     nci.projects.${crateName}.path = ./.;
-    nci.crates.${crateName} = {};
+    nci.crates = {
+      ${crateName} = {
+        drvConfig = {
+          mkDerivation = {
+            buildInputs = packages;
+            shellHook = ''
+              echo "Hello world, tauri with mkDerivation!"
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+          };
+        };
+      };
+    };
 
     devShells.default = config.nci.outputs.${crateName}.devShell.overrideAttrs (old: let
-      libraries = with pkgs; [
-        webkitgtk
-        gtk3
-        cairo
-        gdk-pixbuf
-        glib
-        dbus
-        openssl_3
-        librsvg
-      ];
     in {
-      packages = with pkgs;
-        (old.packages or [])
-        ++ [
-          curl
-          wget
-          pkg-config
-          dbus
-          openssl_3
-          glib
-          gtk3
-          libsoup
-          webkitgtk
-          librsvg
-        ];
-      shellHook = ''
-        ${old.shellHook or ""}
-        echo "Hello world, tauri!"
-        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-        export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-      '';
+      # shellHook = ''
+      #   ${old.shellHook or ""}
+      #   echo "Hello world, tauri!"
+      #   export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+      #   export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+      # '';
     });
   };
 }
