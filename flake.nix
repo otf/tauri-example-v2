a5edd5d466745774dca86bfa2e88212d3ed07452
otf <otf@me.com>
2024-08-10 15:49:02 +0900

    cargo tauri dev, success

--- flake.nix
+++ flake.nix
@@ -23,19 +23,52 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.nci.flakeModule
-        ./src-tauri/crates.nix
+        # ./src-tauri/crates.nix
       ];
       systems = import inputs.systems;
       perSystem = {
         config,
         self',
-        inputs',
+        pkgs,
         ...
       }: let
-        crateOutputs = config.nci.outputs.tauri-app;
+        # crateOutputs = config.nci.outputs.tauri-app;
+        libraries = with pkgs; [
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+        ];
+
+        packages = with pkgs; [
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          librsvg
+        ];
       in {
         # devShells.default = crateOutputs.devShell;
-        packages.default = crateOutputs.packages.release;
+        # packages.default = crateOutputs.packages.release;
+        devShells.default = with pkgs;
+          mkShell {
+            packages = [cargo cargo-tauri nodejs];
+            buildInputs = packages;
+
+            shellHook = ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+          };
       };
       flake = {
         # The usual flake attributes can be defined here.
